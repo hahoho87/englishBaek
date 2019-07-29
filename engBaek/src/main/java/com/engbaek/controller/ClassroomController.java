f628dd1437dedefc05d7fb7bbd4d89d5ca106062
bbaing4564 <bbaing4564@gmail.com>
2019-07-29 19:32:47 +0900

    First Commit

---
+++ engBaek/src/main/java/com/engbaek/controller/ClassroomController.java
@@ -0,0 +1,33 @@
+package com.engbaek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.engbaek.domain.Criteria;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/classroom/*")
+@Log4j
+@AllArgsConstructor
+public class ClassroomController {
+	
+	@GetMapping({ "/read", "/modify" })
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+
+	}
+
+	//TODO BoardVO 변경
+	@PostMapping("/modify")
+	public String modify(BoardVO board, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+		return "redirect:/notice/read";
+	}
+}

4f3363c42eb982d9e1c9eb7d9214f66ae0b5495e
sianpark54 <sianpark54@gmail.com>
2019-07-30 09:53:20 +0900

    xxxxx

---
+++ engBaek/src/main/java/com/engbaek/domain/NoticeVO.java
@@ -0,0 +1,13 @@
+package com.engbaek.domain;
+
+import java.util.Date;
+
+public class NoticeVO {
+
+	private long notice_bno; 
+	private String admin_id;
+	private String notice_title;
+	private String notice_content;
+	private Date notice_regdate;
+	
+}
